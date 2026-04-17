469bd7820598f4d63570597234555da0af270958
tsteelematc <tsteele@madisoncollege.edu>
2026-04-17 14:11:18 -0500

    client side navigation to my component, neat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TsteeleFaves } from './tsteele-faves/tsteele-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "tsteele",
+        component: TsteeleFaves,
+    },
+];
