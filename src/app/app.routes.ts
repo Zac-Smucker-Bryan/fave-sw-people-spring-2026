b8b870660879b04fa08a26ace959d2f4bb47acb6
Colin Bares <colinedwinbares@gmail.com>
2026-04-19 16:52:54 -0500

    Client side navigation to my component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CbaresFaves } from './cbares-faves/cbares-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "cbares",
+        component: CbaresFaves,
+    },
+];
