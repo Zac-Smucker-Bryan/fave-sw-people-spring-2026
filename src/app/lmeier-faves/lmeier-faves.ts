fdee987ffda23237fb6ccee0679e5e318ee49ef5
lmeier127 <Lmeier127@gmail.com>
2026-04-23 18:05:30 -0500

    ng generate component lmeier-faves --skip-tests

---
+++ src/app/lmeier-faves/lmeier-faves.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lmeier-faves',
+  imports: [],
+  templateUrl: './lmeier-faves.html',
+  styleUrl: './lmeier-faves.css',
+})
+export class LmeierFaves {}
