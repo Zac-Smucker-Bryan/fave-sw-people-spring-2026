d290c49e50f2a907757f3dffa548e93e6947f242
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-23 12:28:11 -0500

    client side navigation to my component, neat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ThamiltonFaves } from './thamilton-faves/thamilton-faves';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'thamilton',
+    component: ThamiltonFaves,
+  },
+];
