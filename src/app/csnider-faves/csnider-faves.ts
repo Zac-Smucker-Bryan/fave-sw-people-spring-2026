03186cc2d75cf68ff2175b9a2b46fde7c9ec7a80
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-25 05:33:57 -0500

    Created csnider-faves component

---
+++ src/app/csnider-faves/csnider-faves.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csnider-faves',
+  imports: [],
+  templateUrl: './csnider-faves.html',
+  styleUrl: './csnider-faves.css',
+})
+export class CsniderFaves {}
