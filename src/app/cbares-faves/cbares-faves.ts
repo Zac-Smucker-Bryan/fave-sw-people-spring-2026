2d034dec070b1f2862e48deeff4920ba57e49707
Colin Bares <colinedwinbares@gmail.com>
2026-04-27 11:25:33 -0500

    Promises with async/await

--- src/app/cbares-faves/cbares-faves.ts
+++ src/app/cbares-faves/cbares-faves.ts
@@ -29,4 +29,17 @@ export class CbaresFaves {
       e => console.warn(e)
     )
   }
+
+  protected async promisesWithAsyncAwait() {
+    try {
+
+      const numberOne = await this.swPeopleSvc.getMagicNumber(true);
+      console.log(numberOne);
+
+      const numberTwo = await this.swPeopleSvc.getMagicNumber(false);
+      console.log(numberTwo);
+    } catch(e) {
+      console.warn(e);
+    }
+  }
 }
