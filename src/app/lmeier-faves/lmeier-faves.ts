ffab9bba163081d0985fdcd06c2e909acb48f8ac
lmeier127 <Lmeier127@gmail.com>
2026-04-27 16:59:40 -0500

    showing sw people

--- src/app/lmeier-faves/lmeier-faves.ts
+++ src/app/lmeier-faves/lmeier-faves.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-lmeier-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './lmeier-faves.html',
   styleUrl: './lmeier-faves.css',
 })
-export class LmeierFaves {}
+export class LmeierFaves {
+    private swPeopleSvc = inject(SwPeopleService);
+
+    protected readonly people$ = this.swPeopleSvc.getSwPeople();
+
+
+}
--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SwPeopleService {}
+export class SwPeopleService {
+    private http = inject(HttpClient);
+
+    public getSwPeople() {
+        const fromApi = this.http.get<any>(
+            "https://swapi.info/api/people"
+        );
+
+        // fromApi.subscribe(
+        //     results => console.log(results)
+        // );
+
+        return fromApi;
+    }
+}
