a9c478073aeaa9056487229a16ea9d67cc27f8da
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-28 12:44:31 -0500

    showing sw people, phew....

--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,6 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SwPeopleService {}
+export class SwPeopleService {
+  private http = inject(HttpClient);
+
+  public getSwPeople() {
+    const fromApi = this.http.get<any>('https://swapi.info/api/people');
+    //This was mostly for the purpose of ensuring we got the desired data
+    //fromApi.subscribe((results) => console.log(results));
+    return fromApi;
+  }
+}
--- src/app/thamilton-faves/thamilton-faves.ts
+++ src/app/thamilton-faves/thamilton-faves.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPeopleService } from '../sw-people.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-thamilton-faves',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './thamilton-faves.html',
   styleUrl: './thamilton-faves.css',
 })
-export class ThamiltonFaves {}
+export class ThamiltonFaves {
+  private swPeopleSvc = inject(SwPeopleService);
+
+  //Naming convention: observables get a '$' at the end
+  //hover over a thing to see if it is an observable
+  protected readonly people$ = this.swPeopleSvc.getSwPeople();
+}
