d989a8775b6f87762f2072b7f43024ee055bd3eb
tsteelematc <tsteele@madisoncollege.edu>
2026-05-02 10:46:03 -0500

    one commit for week 15 : - /

--- src/app/tsteele-faves/tsteele-faves.ts
+++ src/app/tsteele-faves/tsteele-faves.ts
@@ -1,17 +1,87 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
-import { AsyncPipe } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
+
+type SwPerson = {
+  name: string;
+  checked: boolean;
+};
 
 @Component({
   selector: 'app-tsteele-faves',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './tsteele-faves.html',
   styleUrl: './tsteele-faves.css',
 })
-export class TsteeleFaves {
+export class TsteeleFaves implements OnInit {
   private swPeopleSvc = inject(SwPeopleService);
 
-  protected readonly people$ = this.swPeopleSvc.getSwPeople();
+  // protected readonly people$ = this.swPeopleSvc.getSwPeople();
+
+  protected readonly swPeople = signal<SwPerson[]>([]);
+
+  protected readonly selectedCount = computed(
+    () => this.swPeople().filter(
+      x => x.checked
+    ).length
+  );
+
+  async ngOnInit() {
+    const people = await firstValueFrom(
+      this.swPeopleSvc.getSwPeople()
+    );
+
+    this.swPeople.set(
+      people.map(
+        (x: any) => ({
+          name: x,
+          checked: false,
+        })
+      )
+    );
+  }
+
+  protected toggleChecked(personToToggle: SwPerson) {
+
+    this.swPeople.set(
+      this.swPeople().map(
+        x => ({
+          ...x,
+          checked: x === personToToggle
+            ? !x.checked
+            : x.checked,
+        })
+      )
+    );
+  }
+
+  protected clearSelected() {
+
+    this.swPeople.set(
+      this.swPeople().map(
+        x => ({
+          ...x,
+          checked: false,
+        })
+      )
+    );
+  }
+
+  protected async postToMsTeams() {
+    await this.swPeopleSvc.postFavesToMsTeams(
+      {
+        name: `Tom's Faves (${this.selectedCount()})`,
+        faves: this.swPeople()
+          .filter(
+            x => x.checked
+          )
+          .map(
+            x => x.name
+          )
+          .join(", "),
+      }
+    );
+  }
 
   protected promisesAsThenables() {
     const numberPromise = this.swPeopleSvc.getMagicNumber(true)
