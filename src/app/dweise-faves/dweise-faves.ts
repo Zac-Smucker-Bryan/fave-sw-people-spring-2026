888ecf7fc8ed547ab895998732efc27564e50bd0
drew-weise <drewweise@gmail.com>
2026-05-05 11:50:25 -0500

    One commit for week 15

--- src/app/dweise-faves/dweise-faves.ts
+++ src/app/dweise-faves/dweise-faves.ts
@@ -1,18 +1,74 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { SwPeopleService } from '../sw-people.service';
-import { AsyncPipe } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
+
+type SwPerson = {
+  name: string;
+  checked: boolean;
+
+};
 
 @Component({
   selector: 'app-dweise-faves',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './dweise-faves.html',
   styleUrl: './dweise-faves.css',
 })
 
-export class DweiseFaves {
+export class DweiseFaves implements OnInit {
   private swPeopleSvc = inject(SwPeopleService);
 
-  protected readonly people$ = this.swPeopleSvc.getSwPeople();
+  // protected readonly people$ = this.swPeopleSvc.getSwPeople();
+
+  protected readonly swPeople = signal<SwPerson[]>([]);
+
+  protected readonly selectedCount = computed(
+    () => this.swPeople().filter(
+      x => x.checked
+    ).length
+  )
+  async ngOnInit() {
+    const people = await firstValueFrom(this.swPeopleSvc.getSwPeople());
+
+    this.swPeople.set(people.map((x: any) => ({name: x, checked: false})));
+  }
+
+  protected toggleChecked(persontoToggle: SwPerson) {
+
+    this.swPeople.set(
+      this.swPeople().map(
+        x => ({
+          ...x,
+          checked: x === persontoToggle
+            ? !x.checked: x.checked,
+        })
+      )
+    );
+  }
+
+  protected clearSelected() {
+
+    this.swPeople.set(
+      this.swPeople().map(
+        x => ({
+          ...x,
+          checked: false,
+        })
+      )
+    );
+  }
+
+  protected async postToMsTeams() {
+    await this.swPeopleSvc.postFavesToMsTeams(
+      {
+        name: `Drew's Faves (${this.selectedCount()})`,
+        faves: this.swPeople()
+          .filter(x => x.checked)
+          .map(x => x.name)
+          .join(", "),
+      }
+    );
+  }
 
   protected promisesAsThenables() {
     this.swPeopleSvc.getMagicNumber(true)
--- src/app/sw-people.service.ts
+++ src/app/sw-people.service.ts
@@ -1,25 +1,58 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { firstValueFrom, map, Observable, tap } from 'rxjs';
+
+export type MsTeamsPostDataShape = {
+  name: string;
+  faves: string;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwPeopleService {
-  private http = inject(HttpClient);
+  private builtInAngularHttpClientService = inject(HttpClient);
 
   public getSwPeople() {
-    const fromApi = this.http.get<any>("https://swapi.info/api/people");
+    const fromApi = this.builtInAngularHttpClientService.get<any>("https://swapi.info/api/people")
+      .pipe(
+        tap(
+          x => console.log(x)
+        ),
+        map(
+          (x: any) => x.map(
+            (y: any) => y.name
+          )
+        ),
+        tap(
+          x => console.log(x)
+        ),
+        map(
+          (x: any) => x.sort(
+            (a: any, b: any) => a.localeCompare(b)
+          )
+        ),
+        tap(
+          x => console.log(x)
+        ),
+      )
+    ;
+    
+    // fromApi.subscribe(
+    //   (results) => console.log(results)
+    // );
+    
+    return fromApi;
+  }
 
-    // fromApi.subscribe(results => console.log(results));
-
-    return fromApi.pipe(
-      tap( x => console.log(x)),
-      map( x => x.map((y: any) => y.name)),
-      tap( x => console.log(x)),
-      map( x => x.sort((a: string, b: string) => a.localeCompare(b))),
-      tap( x => console.log(x)),
-    ); 
+  public postFavesToMsTeams(postData: MsTeamsPostDataShape) {
+    const teamsWebhookUrl = 'https://default33f001466fcc49e9b5687896b3069d.44.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/49bcbafbcb5a4098906af3d90089e2e2/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=-bhnQEdvTmkARlVjn4Yt8OEZhmnwxTR6kaXl9TZWG7I';
+    return firstValueFrom(
+        this.builtInAngularHttpClientService.post(
+            teamsWebhookUrl, 
+            postData,
+        )
+    );
   }
 
   public getMagicNumber(callerWantsToSucceed: boolean): Promise<number> {
@@ -42,3 +75,4 @@ export class SwPeopleService {
     );
   };
 }
+ 
